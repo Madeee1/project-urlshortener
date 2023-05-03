92902c0ad03a448f7d12cc0bb0423977068e2771
Madeee1 <madeee@duck.com>
2023-05-03 06:34:08 +0000

    feat: functions to handle request

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const fs = require('fs');
+const dns = require('dns');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -40,8 +42,60 @@ app.post('/api/shorturl', (req, res) => {
   });
 });
 
-// Function to POST a request to json/url.json
-function postUrl(url, short_url) {
-  
+// Function to get a url from json/url.json, and if it doesn't exist, create a new one and return that
+function getUrl(url) {
+  fs.readFile('./json/url.json', 'utf8', (err, data) => {
+    if (err) {console.log(err);}
+    
+    else {
+      // Search for the url in the json/url.json file
+      const urlDataArray = JSON.parse(data);
+      const urlObject = urlDataArray.find((urlObject) => urlObject.original_url === url);
+      
+      // If the url exists, return its short url form
+      if (urlObject) {
+        return urlObject.short_url;
+      }
+      // If it doesnt exist, create a new one and return it
+      else {
+        const short_url = getLastShortUrl() + 1;
+        postUrl(url, short_url);
+        return short_url;
+      }
+    }
+  });
 }
 
+// Function to write a POST request to json/url.json
+function postUrl(url, short_url) {
+  fs.readFile('./json/url.json', 'utf8', (err, data) => {
+    if (err) {console.log(err);}
+    
+    else {
+      const urlData = JSON.parse(data);
+      urlData.push({url: url, short_url: short_url});
+      
+      fs.writeFile('./json/url.json', JSON.stringify(urlData), (err) => {
+        if (err) {
+          console.log(err);
+        }
+        else {
+          console.log('Data written to file');
+        }
+      });
+    }
+  });
+}
+
+// Function get the last short_url in the json/url.json file
+function getLastShortUrl() {
+  fs.readFile('./json/url.json', 'utf8', (err, data) => {
+    if (err) {console.log(err);}
+    
+    else {
+      const urlData = JSON.parse(data);
+      const lastShortUrl = urlData[urlData.length - 1].short_url;
+      return lastShortUrl;
+    }
+  });
+}
