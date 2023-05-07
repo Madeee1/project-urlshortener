cd1983d0dd3fd9cdd3103a2abdd5671a83402037
Madeee1 <madeee@duck.com>
2023-05-07 12:52:08 +0000

    feat: multiple mongoose functions

--- index.js
+++ index.js
@@ -48,3 +48,57 @@ const mongoose = require('mongoose');
 
 // Connect to database
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Create schema, both being required
+const urlSchema = new mongoose.Schema({
+  original_url: { type: String, required: true, unique: true },
+  short_url: { type: Number, required: true, unique: true }
+});
+
+// Create model
+const Url = mongoose.model('Url', urlSchema);
+
+// Function to get the next available short_url
+async function getNextShortUrl() {
+  try {
+    const data = await Url.find({});
+    return data.length + 1;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+
+// Function to add a new url to the database
+async function addUrl(url) {
+  try {
+    const shortUrl = await getNextShortUrl();
+    const newUrl = new Url({ original_url: url, short_url: shortUrl });
+    const savedUrl = await newUrl.save();
+    console.log("Saved url: " + savedUrl);
+    return savedUrl;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+// Function to get a url from the database
+async function getUrl(shortUrl) {
+  try {
+    const url = await Url.findOne({ short_url: shortUrl });
+    return url ? url.original_url : null;
+  } catch (err) {
+    console.log(err);
+    return null;
+  }
+}  
+
+// Function to check if a url is already in the database
+async function urlExists(url) {
+  try {
+    const data = await Url.find({ original_url: url });
+    return data.length > 0;
+  } catch (err) {
+    console.log(err);
+  }
+}
